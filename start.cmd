e111c727e95f575d4d0c1b8904d3024d15bcef6e
JanezRadescek <janez.radescek@gmail.com>
2020-04-20 13:13:29 +0200

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -3,6 +3,8 @@ echo varport
 START .\elevator\SimElevatorServer --port %varport%
 START go run main.go %varport%
 
+timeout 2
+
 set varport=varport+1
 echo varport
 START .\elevator\SimElevatorServer --port %varport%
